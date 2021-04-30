a90096adc46c28202266b36e85c0fe0aae75eeff
Michael Bargatin <attractadore02@gmail.com>
2021-04-30 18:41:55 +0300

    DoubleList WIP

---
+++ include/DataStructures/BaseDoubleList.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <stddef.h>
+
+typedef struct DoubleList_T DoubleList;
+
+typedef struct node_t DoubleListNode;
+
+DoubleList* doubleListAlloc(size_t key_size);
+void doubleListFree(DoubleList* list);
+
+DoubleListNode* doubleListAddFront(DoubleList* list, void const* value);
+void doubleListPrepend(DoubleList* list, DoubleListNode* node);
+
+DoubleListNode* doubleListRemove(DoubleList* list, DoubleListNode* node);
+void doubleListPopBack(DoubleList* list);
+
+DoubleListNode* doubleListMoveToFront(DoubleList* list, DoubleListNode* node);
