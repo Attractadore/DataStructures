b79bb85d1fed427b49c8ed17ff74c4e11537c617
Alpatova Polina <70851522+PolinaA2020@users.noreply.github.com>
2021-05-10 18:56:10 +0300

    Some codestyle changes

--- src/BaseDoubleList.c
+++ src/BaseDoubleList.c
@@ -32,7 +32,8 @@ typedef enum type {
 void doubleListShowList(DoubleList* list, TypeOfValue type);
 
 /*------------------------------------------------------------------------------------------------------------------------------*/
-DoubleList* doubleListAlloc(size_t key_size) {
+DoubleList* doubleListAlloc(size_t key_size)
+{
     DoubleList* list = calloc(1, sizeof(DoubleList));
     if (!list)
         return NULL;
@@ -41,7 +42,8 @@ DoubleList* doubleListAlloc(size_t key_size) {
     return list;
 }
 /*------------------------------------------------------------------------------------------------------------------------------*/
-DoubleListNode* doubleListAddFront(DoubleList* list, void const* value) {
+DoubleListNode* doubleListAddFront(DoubleList* list, void const* value)
+{
     DoubleListNode* node = calloc(1, sizeof(DoubleListNode) + list->key_size);
     if (!node)
         return NULL;
@@ -50,7 +52,8 @@ DoubleListNode* doubleListAddFront(DoubleList* list, void const* value) {
     return doubleListPrepend(list, node);
 }
 /*------------------------------------------------------------------------------------------------------------------------------*/
-DoubleListNode* doubleListPrepend(DoubleList* list, DoubleListNode* node) {
+DoubleListNode* doubleListPrepend(DoubleList* list, DoubleListNode* node)
+{
     assert(node);
     if (list->start == NULL) {
         list->start = node;
@@ -63,29 +66,32 @@ DoubleListNode* doubleListPrepend(DoubleList* list, DoubleListNode* node) {
     list->length++;  // update cur_size
     return list->start;
 }
-
-DoubleListNode* doubleListPopFront(DoubleList* list) {
+/*------------------------------------------------------------------------------------------------------------------------------*/
+DoubleListNode* doubleListPopFront(DoubleList* list)
+{
     assert(list && list->start);
 
     doubleListDelete(list, list->start);
     return list->start;
 }
-
 /*------------------------------------------------------------------------------------------------------------------------------*/
-DoubleListNode* doubleListPopBack(DoubleList* list) {
+DoubleListNode* doubleListPopBack(DoubleList* list)
+{
     assert(list && list->end && "DoubleList is already empty!");
 
     doubleListDelete(list, list->end);
     return list->end;
 }
 /*------------------------------------------------------------------------------------------------------------------------------*/
-DoubleListNode* doubleListMoveToFront(DoubleList* list, DoubleListNode* node) {
+DoubleListNode* doubleListMoveToFront(DoubleList* list, DoubleListNode* node)
+{
     doubleListPrepend(list, doubleListRemove(list, node));
 
     return list->start;
 }
 /*------------------------------------------------------------------------------------------------------------------------------*/
-void doubleListShowList(DoubleList* list, TypeOfValue type) {
+void doubleListShowList(DoubleList* list, TypeOfValue type)
+{
     if (list->start == NULL)
         printf("List is empty!\n");
     else {
@@ -122,9 +128,9 @@ void doubleListShowList(DoubleList* list, TypeOfValue type) {
         printf("\n");
     }
 }
-
 /*------------------------------------------------------------------------------------------------------------------------------*/
-DoubleListNode* doubleListRemove(DoubleList* list, DoubleListNode* node) {
+DoubleListNode* doubleListRemove(DoubleList* list, DoubleListNode* node)
+{
     assert(node && "Error: node == nullptr");
 
     if (node->prev) {
@@ -145,35 +151,41 @@ DoubleListNode* doubleListRemove(DoubleList* list, DoubleListNode* node) {
 
     return node;
 }
-
-void doubleListDelete(DoubleList* list, DoubleListNode* node) {
+/*------------------------------------------------------------------------------------------------------------------------------*/
+void doubleListDelete(DoubleList* list, DoubleListNode* node)
+{
     assert(list && node);
     free(doubleListRemove(list, node));
 }
 /*------------------------------------------------------------------------------------------------------------------------------*/
-size_t doubleListSize(DoubleList const* list) {
+size_t doubleListSize(DoubleList const* list)
+{
     assert(list);
 
     return list->length;
 }
 /*------------------------------------------------------------------------------------------------------------------------------*/
-void const* doubleListNodeConstData(DoubleListNode const* node) {
+void const* doubleListNodeConstData(DoubleListNode const* node)
+{
     assert(node);
 
     return node->data;
 }
 /*------------------------------------------------------------------------------------------------------------------------------*/
-DoubleListNode const* doubleListConstBack(DoubleList const* list) {
+DoubleListNode const* doubleListConstBack(DoubleList const* list)
+{
     assert(list);
 
     return list->end;
 }
 /*------------------------------------------------------------------------------------------------------------------------------*/
-size_t doubleListItemSize(DoubleList const* list) {
+size_t doubleListItemSize(DoubleList const* list)
+{
     return list->key_size;
 }
 /*------------------------------------------------------------------------------------------------------------------------------*/
-void doubleListFree(DoubleList* list) {
+void doubleListFree(DoubleList* list)
+{
     if (list) {
         DoubleListNode* tmp = list->start;
         DoubleListNode* tmp_next;
