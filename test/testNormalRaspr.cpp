e6b4de6c79a79bfbc1269d49f7806bf078637c1a
Alpatova Polina <alpatova.pg@phystech.edu>
2021-05-16 16:41:57 +0300

    New Tests added

--- test/testNormalRaspr.cpp
+++ test/testNormalRaspr.cpp
@@ -7,27 +7,17 @@
 
 int main()
 {
-    /*int E, D;
-    cin >> E >> D;
+    int E, D, n;
+    std::cin >> E >> D >> n;
     std::random_device rd;
     std::mt19937 gen(rd());
     std::normal_distribution<> X(E, D);
-    std::vector<int> data(10);
+    std::vector<int> data(n);
+    std::cout << n << '\n';
     for (int& var : data) {
         var = X(gen);
         std::cout << var << '\n';
     }
-    */
-    int n = 15;
-    for(i = 0; i < n; i++)
-        std::cout<< PoissonKnuth(10)<<'\n';
+
 }
 
-double PoissonKnuth(double rate) {
-    double k = 0, prod = Uniform(0, 1);
-    while (prod > exp(-rate) {
-        prod *= Uniform(0, 1);
-        ++k;
-    }
-    return k;
-}
