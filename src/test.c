531d77318fab3370a8a68d4d5051986905b925f7
Alpatova Polina <alpatova.pg@phystech.edu>
2021-05-16 22:20:36 +0300

    Tests for monoList

---
+++ src/test.c
@@ -0,0 +1,16 @@
+#include "MonoList.c"
+
+void test_monoListMoveToFrontOneElement() {
+    l = List();
+    l.append();
+    node = l.back();
+    l.moveToFront();
+    TEST_ASSERT_PTR_EQUAL(l.start, node);
+    TEST_ASSERT_PTR_EQUAL(l.end, node);
+    TEST_ASSERT_PTR_EQUAL(node->next, NULL);
+}
+
+void test_monoListAppend() {
+    MonoList* l = monoListAlloc(sizeof(int));
+
+}
