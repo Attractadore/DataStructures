fb5aa452df59c3d1535d7442ee956ecd6cbc469b
trotrotro <70337484+trotrotro@users.noreply.github.com>
2021-05-16 22:35:58 +0300

    Update BaseMonoList.h

--- include/DataStructures/BaseMonoList.h
+++ include/DataStructures/BaseMonoList.h
@@ -4,41 +4,45 @@
 
 typedef struct MonoList_T MonoList;
 typedef struct MonoListNode_t MonoListNode;
-
-MonoList *monoListAlloc(size_t key_size);
 // allocates a list
-void monoListFree(MonoList *ml);
+MonoList *monoListAlloc(size_t key_size);
 // frees a list
+void monoListFree(MonoList *ml);
+// allocates a new node and puts it to the front as a start, returns list->start
 MonoListNode *monoListAddToFront(MonoList *ml, void const *value);
-// allocates a new node and puts it to the front as a start
+// allocates a new node and puts it to the back as an end, returns list->end
 MonoListNode *monoListAddToBack(MonoList *ml, void const *value);
-// allocates a new node and puts it to the back as an end
+// removes the node from the back and deletes it, returns list->end
 MonoListNode *monoListPopBack(MonoList *ml);
-// removes the node from the back and deletes it
+// removes the node from the front and deletes it, returns list->start
 MonoListNode *monoListPopFront(MonoList *ml);
-// removes the node from the front and deletes it
+// removes the next node and puts it to the front as a start, returns list->start
 MonoListNode *monoListMoveNextToFront(MonoList *ml, MonoListNode *prenode);
-// removes the next node and puts it to the front as a start
+// removes the next node and puts it to the back as an end, returns list->end
 MonoListNode *monoListMoveNextToBack(MonoList *ml, MonoListNode *prenode);
-// removes the next node and puts it to the back as an end
-MonoListNode *monoListRemoveNext(MonoList *ml, MonoListNode *prenode);
 // removes the next node without deleting, returns pointer to the removed node
-MonoListNode *monoListDeleteNext(MonoList *ml, MonoListNode *prenode);
+MonoListNode *monoListRemoveNext(MonoList *ml, MonoListNode *prenode);
 // removes the next node and deletes it, returns new prenode->next
-MonoListNode *monoListFront(MonoList *ml);
+MonoListNode *monoListDeleteNext(MonoList *ml, MonoListNode *prenode);
 // returns list->start
-MonoListNode *monoListBack(MonoList *ml);
+MonoListNode *monoListFront(MonoList *ml);
 // returns list->end
-size_t monoListSize(MonoList *ml);
+MonoListNode *monoListBack(MonoList *ml);
 // returns list->length
-size_t monoListItemSize(MonoList *ml);
+size_t monoListSize(MonoList *ml);
 // returns list->key_size
-void *monoListNodeData(MonoListNode *node);
+size_t monoListItemSize(MonoList *ml);
 // returns node->data
+void *monoListNodeData(MonoListNode *node);
+// returns const list->start
 MonoListNode const *monoListConstFront(MonoList const *ml);
-// here and further - the same, but everything is const
+// returns const list->end
 MonoListNode const *monoListConstBack(MonoList const *ml);
+// returns const node->data
 void const *monoListConstNodeData(MonoListNode const *node);
+// makes node be a new front, returns list->start
 MonoListNode *monoListPrepend(MonoList *ml, MonoListNode *node);
+// makes node be a new back, returns list->end
 MonoListNode *monoListAppend(MonoList *ml, MonoListNode *node);
+// returns node->next
 MonoListNode *monoListNodeNext(MonoListNode *node);
